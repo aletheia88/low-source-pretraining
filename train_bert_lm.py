09389d5708ab0656f0f11c5a2e2f68604f1a281d
kunyanglu <lualicia88@gmail.com>
2022-04-07 00:32:01 -0700

    small shifts

--- train_bert_lm.py
+++ train_bert_lm.py
@@ -19,7 +19,7 @@ MAX_LEN = 1320
 MASKING_PROPORTION = 0.15
 BATCH_SIZE = 4
 
-class CustomDataset(Dataset):
+class Dataset(Dataset):
     
     def __init__(self, df, tokenizer, max_length):
         self.examples = []
@@ -28,7 +28,10 @@ class CustomDataset(Dataset):
             words = codewords.split()
             chunks = [' '.join(words[i:i+max_length]) for i in range(0, len(words), max_length)]
             for example in chunks:
-                x = tokenizer.encode_plus(example, max_length=max_length, padding='max_length', truncation=True)
+                x = tokenizer.encode_plus(example,
+                                        max_length=max_length,
+                                        padding='max_length',
+                                        truncation=True)
                 assert(len(x.input_ids) == max_length)
                 self.examples += [x.input_ids]
 
@@ -38,14 +41,28 @@ class CustomDataset(Dataset):
     def __getitem__(self, i):
         return torch.tensor(self.examples[i])
 
+def train_LM(ds_name):
+    
+    tokenizer = RobertaTokenizerFast.from_pretrained(TOKENIZER_SAVEDIR)
+    train_dataset, valid_dataset = create_train_val_set(ds_name,
+                                    tokenizer)
+    data_collator = create_data_collector(tokenizer)
+    create_train_bert_lm(data_collator, train_dataset, valid_dataset)
+
+def fine_tune_LM(model_path):
+    """Not sure how to fine-tune"""
+
+    #model = RobertaForMaskedLM.from_pretrained(model_path)
+    state_dict = torch.load('bert-models/pytorch_model.bin')
+
 def create_train_val_set(file_name, tokenizer):
 
     df = pd.read_csv(file_name)
     train_df = df[df['is_valid'] == 0]
     valid_df = df[df['is_valid'] == 1]
 
-    train_dataset = CustomDataset(train_df['codewords'], tokenizer, MAX_LEN)
-    valid_dataset = CustomDataset(valid_df['codewords'], tokenizer, MAX_LEN)
+    train_dataset = Dataset(train_df['codewords'], tokenizer, MAX_LEN)
+    valid_dataset = Dataset(valid_df['codewords'], tokenizer, MAX_LEN)
 
     return train_dataset, valid_dataset
 
@@ -97,12 +114,15 @@ def create_train_bert_lm(data_collator, train_dataset, valid_dataset):
 
 if __name__ == "__main__":
 
-    torch.cuda.set_device(1)
-    print('Cuda available: ', torch.cuda.is_available())
-    tokenizer = RobertaTokenizerFast.from_pretrained(TOKENIZER_SAVEDIR)
-    train_dataset, valid_dataset = create_train_val_set('train_val_data.csv',
-                                                                    tokenizer)
-    data_collator = create_data_collector(tokenizer)
-    create_train_bert_lm(data_collator, train_dataset, valid_dataset)
+    #train_LM('train_val_data.csv')
+    model_path = 'bert-models'
+    model = RobertaForMaskedLM.from_pretrained(model_path)
+    parameters = model.parameters()
 
+    for param in parameters:
+        print(param)
+        
+    state_dict = torch.load('bert-models/pytorch_model.bin')
+    print('state_dict')
+    print(state_dict)
 
